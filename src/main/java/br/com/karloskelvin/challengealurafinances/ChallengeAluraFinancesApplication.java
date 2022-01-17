fe82844756d77ed84bd7cd2cd9c7c48fcf272afd
Karlos Kelvin <karloskelvinsantos@gmail.com>
2022-01-17 12:16:44 -0300

    project created with spring boot and mongodb

---
+++ src/main/java/br/com/karloskelvin/challengealurafinances/ChallengeAluraFinancesApplication.java
@@ -0,0 +1,13 @@
+package br.com.karloskelvin.challengealurafinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraFinancesApplication.class, args);
+	}
+
+}
