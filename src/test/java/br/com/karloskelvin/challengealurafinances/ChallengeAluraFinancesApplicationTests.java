b06067dff31d660511ba47ee44ed41281b6d019b
Karlos Kelvin <karloskelvinsantos@gmail.com>
2022-01-17 12:17:57 -0300

    add class sample teste

---
+++ src/test/java/br/com/karloskelvin/challengealurafinances/ChallengeAluraFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.karloskelvin.challengealurafinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
